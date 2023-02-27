aa2e09a62e36e7aa08fdbf3e20202aa7d2846c6f
Matthieu-Simon <simon.matthieu89@gmail.com>
2023-02-27 11:20:25 +0100

    Add button filter homepage

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,4 +1,5 @@
 const gallery = document.querySelector(".gallery");
+const titlePortfolio = document.querySelector(".title-portfolio");
 
 const dataApi = fetch('http://localhost:5678/api/works');
 
@@ -18,7 +19,9 @@ dataApi.then (response => {
             gallery.appendChild(figure);
 
             let imgWorks = document.createElement("img");
-            imgWorks.src = "./assets/images/abajour-tahina.png";
+            // imgWorks.src = "./assets/images/abajour-tahina.png";
+            // imgWorks.src = work.imageUrl;
+            // console.log(work.imageUrl)
             imgWorks.setAttribute("alt", work.title);
             figure.appendChild(imgWorks);
 
@@ -27,4 +30,35 @@ dataApi.then (response => {
             figure.appendChild(figcaption);
         }
         
-    });
+});
+
+// Ajout des boutons de filtre
+const categoriesApi = fetch('http://localhost:5678/api/categories');
+
+categoriesApi.then (response => {
+    if (response.ok) {
+        return response.json();
+    } else {
+        throw new Error('Request failed!');
+    }
+    })
+    .then (categories => {
+        for (let i = 0; i < categories.length; i++) {
+            let category = categories[i];
+
+            let button = document.createElement("button");
+            button.innerHTML = category.name;
+            button.classList.add("btn");
+            divFilter.appendChild(button);
+        }
+});
+// Ajout de la div contenant les boutons de filtre
+const divFilter = document.createElement("div");
+divFilter.innerHTML = "";
+divFilter.classList.add("div-btn");
+titlePortfolio.append(divFilter);
+
+const btnAll = document.createElement("button");
+btnAll.innerHTML = "Tous";
+btnAll.classList.add("btn");
+divFilter.appendChild(btnAll);
