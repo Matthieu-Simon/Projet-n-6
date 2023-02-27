468dd2de145f4b4c3e3505176f5ce65150012ab9
Matthieu-Simon <simon.matthieu89@gmail.com>
2023-02-27 13:04:32 +0100

    fix: correction requete api images homepage

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,5 +1,12 @@
+
+
 const gallery = document.querySelector(".gallery");
 const titlePortfolio = document.querySelector(".title-portfolio");
+
+function urlToPath(url) {
+    const splitUrl = url.split("/");
+    return splitUrl[splitUrl.length - 1].replace(/[0-9]/g,"");
+}
 
 const dataApi = fetch('http://localhost:5678/api/works');
 
@@ -11,17 +18,13 @@ dataApi.then (response => {
     }
     })
     .then (data => {
-        for (i = 0; i < data.length; i++) {
-            let work = data[i];
-
+        for (const work of data) {
             // on crée les éléments html
             let figure = document.createElement("figure");
             gallery.appendChild(figure);
 
             let imgWorks = document.createElement("img");
-            // imgWorks.src = "./assets/images/abajour-tahina.png";
-            // imgWorks.src = work.imageUrl;
-            // console.log(work.imageUrl)
+            imgWorks.src = "./assets/images/" + urlToPath(work.imageUrl);
             imgWorks.setAttribute("alt", work.title);
             figure.appendChild(imgWorks);
 
@@ -29,9 +32,11 @@ dataApi.then (response => {
             figcaption.innerHTML = work.title;
             figure.appendChild(figcaption);
         }
-        
 });
 
+const myfunction = (id) => {
+    console.log(id);
+}
 // Ajout des boutons de filtre
 const categoriesApi = fetch('http://localhost:5678/api/categories');
 
@@ -46,19 +51,52 @@ categoriesApi.then (response => {
         for (let i = 0; i < categories.length; i++) {
             let category = categories[i];
 
-            let button = document.createElement("button");
-            button.innerHTML = category.name;
-            button.classList.add("btn");
-            divFilter.appendChild(button);
+            let btnFilter = document.createElement("button");
+            btnFilter.innerHTML = category.name;
+            btnFilter.setAttribute("id", category.id);
+            btnFilter.classList.add("btn");
+            divFilterWorks.appendChild(btnFilter);
+
+            btnFilter.addEventListener('click', () => {
+                myfunction(category.id);
+            })
         }
 });
 // Ajout de la div contenant les boutons de filtre
-const divFilter = document.createElement("div");
-divFilter.innerHTML = "";
-divFilter.classList.add("div-btn");
-titlePortfolio.append(divFilter);
+const divFilterWorks = document.createElement("div");
+divFilterWorks.innerHTML = "";
+divFilterWorks.classList.add("div-btnFilter");
+titlePortfolio.append(divFilterWorks);
 
-const btnAll = document.createElement("button");
-btnAll.innerHTML = "Tous";
-btnAll.classList.add("btn");
-divFilter.appendChild(btnAll);
+const btnAllWorks = document.createElement("button");
+btnAllWorks.innerHTML = "Tous";
+btnAllWorks.setAttribute("id", "all");
+btnAllWorks.classList.add("btn");
+divFilterWorks.appendChild(btnAllWorks);
+
+// Ajout de la fonctionnalité de filtre
+function showGalleryFiltered (filteredWorks) {
+    gallery.innerHTML = "";
+    for (let key in filteredWorks) {
+        let work = filteredWorks[key];
+        let figure = document.createElement("figure");
+        gallery.appendChild(figure);
+
+        let imgWorks = document.createElement("img");
+        imgWorks.src = work.imageUrl;
+        imgWorks.setAttribute("alt", work.title);
+        figure.appendChild(imgWorks);
+
+        let figcaption = document.createElement("figcaption");
+        figcaption.innerHTML = work.title;
+        figure.appendChild(figcaption);
+    }
+}
+
+
+btnAllWorks.addEventListener("click", function() {
+    
+    console.log("Tous les travaux");
+    // showGalleryFiltered(filteredData);
+});
+
---
+++ FrontEnd/utils.js
@@ -0,0 +1,4 @@
+function urlToPath(url) {
+    const splitUrl = url.split("/");
+    console.log(splitUrl);
+}
